794a18fa886365564697c276006d7622358b190d
Alvaro Avalos <tlalvaro15@gmail.com>
2022-10-21 15:37:34 -0500

    controller from singleton

--- server/src/controllers/products/index.js
+++ server/src/controllers/products/index.js
@@ -1,17 +1,11 @@
+import persistence from "../index.js"
+const { productsDAO } = persistence
 const getProducts = async (req, res) => {
   try {
+    const products = await productsDAO.getAll()
     res.status(200).json({
       message: "OK",
-      products: [
-        {
-          _id: 1,
-          product: "product1",
-        },
-        {
-          _id: 2,
-          product: "product2",
-        },
-      ],
+      products: products,
     })
   } catch (error) {
     res.status(500).json({
