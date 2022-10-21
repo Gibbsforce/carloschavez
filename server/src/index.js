df849de32f5068e62738dac2db92cfe422e1f983
Alvaro Avalos <tlalvaro15@gmail.com>
2022-10-21 17:29:59 -0500

    json encoded added

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,8 @@ import express from "express"
 import router from "./routers/index.js"
 const PORT = process.env.PORT
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use("/api/v1", router)
 app.listen(PORT, () =>
   console.log(`server running on PORT ${PORT}, url: http://localhost:${PORT}`)
