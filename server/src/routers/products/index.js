83dfdb4ff8513a9c15758fc3165dac9d81c4d3c3
Alvaro Avalos <tlalvaro15@gmail.com>
2022-10-21 17:31:15 -0500

    router from controllers added

--- server/src/routers/products/index.js
+++ server/src/routers/products/index.js
@@ -4,4 +4,9 @@ import productsController from "../../controllers/products/index.js"
 const productsRouter = express.Router()
 // Routers
 productsRouter.get("/", productsController.getProducts)
+productsRouter.post("/", productsController.createProduct)
+productsRouter.get("/:id", productsController.getProductById)
+productsRouter.patch("/:id", productsController.updateProductById)
+productsRouter.delete("/:id", productsController.deleteProductById)
+productsRouter.delete("/", productsController.deleteProducts)
 export default productsRouter
