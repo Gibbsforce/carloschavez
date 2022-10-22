e2f2593e5755a06b5e6fbe975df546e8cc0d7c3f
Alvaro Avalos <tlalvaro15@gmail.com>
2022-10-22 12:02:28 -0500

    routering the users

--- server/src/routers/index.js
+++ server/src/routers/index.js
@@ -1,7 +1,9 @@
 import express from "express"
 // Routes
 import productsRouter from "./products/index.js"
+import usersRouter from "./users/index.js"
 // Global router
 const router = express.Router()
 router.use("/products", productsRouter)
+router.use("/auth", usersRouter)
 export default router
