39dec0f8ab7b4f50c0c8021ed137cc75c0157e57
Alvaro Avalos <tlalvaro15@gmail.com>
2022-10-22 22:42:05 -0500

    added the middlewares to the products router and finish server for now

--- server/src/routers/products/index.js
+++ server/src/routers/products/index.js
@@ -1,12 +1,33 @@
 import express from "express"
 // Controllers
 import productsController from "../../controllers/products/index.js"
+// Middlewares
+import authenticateToken from "../../middlewares/users/authenticateToken.js"
+import {
+  createProductValidation,
+  updateProductValidation,
+} from "../../middlewares/products/productsValidation.js"
+// Defining router
 const productsRouter = express.Router()
 // Routers
 productsRouter.get("/", productsController.getProducts)
-productsRouter.post("/", productsController.createProduct)
+productsRouter.post(
+  "/",
+  authenticateToken,
+  createProductValidation,
+  productsController.createProduct
+)
 productsRouter.get("/:id", productsController.getProductById)
-productsRouter.patch("/:id", productsController.updateProductById)
-productsRouter.delete("/:id", productsController.deleteProductById)
-productsRouter.delete("/", productsController.deleteProducts)
+productsRouter.patch(
+  "/:id",
+  authenticateToken,
+  updateProductValidation,
+  productsController.updateProductById
+)
+productsRouter.delete(
+  "/:id",
+  authenticateToken,
+  productsController.deleteProductById
+)
+productsRouter.delete("/", authenticateToken, productsController.deleteProducts)
 export default productsRouter
