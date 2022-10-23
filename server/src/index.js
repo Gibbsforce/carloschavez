c03902f06529fc0c7e9c88837c4caefed70cb3eb
Alvaro Avalos <tlalvaro15@gmail.com>
2022-10-22 22:46:11 -0500

    cors added to server

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,10 @@
 import { dotenv } from "./config.js"
 import express from "express"
+import cors from "cors"
 import router from "./routers/index.js"
 const PORT = process.env.PORT
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use("/api/v1", router)
