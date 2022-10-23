5bed0fdd96e8d1059316c0558e9a79c8e5df1fe9
Alvaro Avalos <tlalvaro15@gmail.com>
2022-10-23 12:21:53 -0500

    api config done

---
+++ client/src/config.js
@@ -0,0 +1,19 @@
+// API development variables
+const SSL = false
+const API_PORT = 8080
+const API_VERSION = "v1"
+const API_PROTOCOL = SSL === true ? "https" : "http"
+const API_HOST = "localhost"
+
+// API
+const API_URL = `${API_PROTOCOL}://${API_HOST}:${API_PORT}/api/${API_VERSION}`
+
+// products endpoint
+const PRODUCTS = `${API_URL}/products`
+
+// users and auth endpoint
+const AUTH_SIGNUP = `${API_URL}/auth/signup`
+const AUTH_LOGIN = `${API_URL}/auth/login`
+const AUTH_USER = `${API_URL}/auth/user`
+
+export { PRODUCTS, AUTH_SIGNUP, AUTH_LOGIN, AUTH_LOGIN }
