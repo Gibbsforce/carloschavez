4f47aa73dd2b0412a28a85c1913638a5d3146840
Alvaro Avalos <tlalvaro15@gmail.com>
2022-10-23 16:26:51 -0500

    headeer added

--- client/src/utils/navbarItems.js
+++ client/src/utils/navbarItems.js
@@ -1,18 +1,6 @@
 export const NAV_ITEMS = [
   {
     label: "Inicio",
-    children: [
-      {
-        label: "Explore Design Work",
-        subLabel: "Trending Design to inspire you",
-        href: "#",
-      },
-      {
-        label: "New & Noteworthy",
-        subLabel: "Up-and-coming Designers",
-        href: "#",
-      },
-    ],
   },
   {
     label: "Productos",
