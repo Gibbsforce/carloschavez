d3216dcf213d2c4f49cee6ce724c4224ef951987
Alvaro Avalos <tlalvaro15@gmail.com>
2022-10-22 22:16:33 -0500

    middlewares added to users router

--- server/src/routers/users/index.js
+++ server/src/routers/users/index.js
@@ -3,11 +3,15 @@ import express from "express"
 import usersController from "../../controllers/users/index.js"
 // Middlewares
 import authenticateToken from "../../middlewares/users/authenticateToken.js"
+import {
+  validateSignUp,
+  validateLogin,
+} from "../../middlewares/users/userValidation.js"
 // Defining router
 const usersRouter = express.Router()
 // Routers
-usersRouter.post("/signup", usersController.signUp)
-usersRouter.post("/login", usersController.login)
+usersRouter.post("/signup", validateSignUp, usersController.signUp)
+usersRouter.post("/login", validateLogin, usersController.login)
 usersRouter.get("/user", authenticateToken, usersController.getUser)
 usersRouter.delete("/user", authenticateToken, usersController.deleteUser)
 export default usersRouter
