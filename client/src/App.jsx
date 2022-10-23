ff13aaf5462e51f701814aa70b1498613e3d09e9
Alvaro Avalos <tlalvaro15@gmail.com>
2022-10-23 12:01:36 -0500

    chakra scheme theme defined

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
+// chakra test
+import { Box } from "@chakra-ui/react"
 // Routing
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div>Hello world!</div>} />
+        <Route path="/" element={<Box color="primary">Hello world!</Box>} />
       </Routes>
     </Router>
   )
---
+++ client/src/styles/theme.js
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react"
+export const companyColors = extendTheme({
+  colors: {
+    primary: "#845EC2",
+    secondary: "#FF6F91",
+    highlight: "#00C9A7",
+    warning: "#FFC75F",
+    danger: "#C34A36",
+  },
+})
