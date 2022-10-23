53031bdb6fa0e690de05e10dcb61b5c7c3f3d55f
Alvaro Avalos <tlalvaro15@gmail.com>
2022-10-23 16:25:08 -0500

    headeer added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 // chakra test
 import { Box } from "@chakra-ui/react"
+// Components
+import Header from "./components/Header"
 // Routing
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 const App = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Box color="primary">Hello world!</Box>} />
       </Routes>
---
+++ client/src/utils/navbarItems.js
@@ -0,0 +1,40 @@
+export const NAV_ITEMS = [
+  {
+    label: "Inicio",
+    children: [
+      {
+        label: "Explore Design Work",
+        subLabel: "Trending Design to inspire you",
+        href: "#",
+      },
+      {
+        label: "New & Noteworthy",
+        subLabel: "Up-and-coming Designers",
+        href: "#",
+      },
+    ],
+  },
+  {
+    label: "Productos",
+    children: [
+      {
+        label: "Listar todos",
+        subLabel: "Encuentra tu producto",
+        href: "#",
+      },
+      {
+        label: "Comprar",
+        subLabel: "Ir directamente a comprar",
+        href: "#",
+      },
+    ],
+  },
+  {
+    label: "Sobre nosotros",
+    href: "#",
+  },
+  {
+    label: "Contacto",
+    href: "#",
+  },
+]
