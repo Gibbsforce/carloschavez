6bcc13389ab029e001237429142c72c39ec9da26
Alvaro Avalos <tlalvaro15@gmail.com>
2022-10-22 22:47:04 -0500

    helmet added to server

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,12 @@
 import { dotenv } from "./config.js"
 import express from "express"
 import cors from "cors"
+import helmet from "helmet"
 import router from "./routers/index.js"
 const PORT = process.env.PORT
 const app = express()
 app.use(cors())
+app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use("/api/v1", router)
