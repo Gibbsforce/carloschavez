d217c3118d8c04f1467e203c8a3939f4f6f8ad0a
Alvaro Avalos <tlalvaro15@gmail.com>
2022-10-23 23:14:08 -0500

    data test for db of products

---
+++ client/src/utils/dataTest.js
@@ -0,0 +1,43 @@
+const dataTest = [
+  {
+    product_name: "product_name",
+    product_image:
+      "https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+    product_description: "product_description",
+    product_price: 80,
+    product_price_discount: 50,
+  },
+  {
+    product_name: "product_name",
+    product_image:
+      "https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+    product_description: "product_description",
+    product_price: 80,
+    product_price_discount: 50,
+  },
+  {
+    product_name: "product_name",
+    product_image:
+      "https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+    product_description: "product_description",
+    product_price: 80,
+    product_price_discount: 50,
+  },
+  {
+    product_name: "product_name",
+    product_image:
+      "https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+    product_description: "product_description",
+    product_price: 80,
+    product_price_discount: 50,
+  },
+  {
+    product_name: "product_name",
+    product_image:
+      "https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+    product_description: "product_description",
+    product_price: 80,
+    product_price_discount: 50,
+  },
+]
+export default dataTest
